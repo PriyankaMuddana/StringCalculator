195014eb69c4aa02015712c7d56ee369a07f2a97
Priyanka Muddana <priyankamuddana.it@gmail.com>
2024-06-29 18:15:26 +0530

    Improved class method to match the behaviour and added delimiter spec

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,21 @@ class StringCalculator
   def self.add(string)
     return "String is empty" if string.empty?
     return "Input must be a String" unless string.class == String
+
+    if string.start_with?('//')
+      delimiter_spec, numbers = string.split("\n", 2)
+      custom_delimiter = delimiter_spec[2..]
+      delimiter = Regexp.escape(custom_delimiter)
+    else
+      numbers = string
+    end
+
+    delimiter = /,|\n/
+    number_set = numbers.split(/#{delimiter}/).map(&:to_i)
+
+    negative_numbers = number_set.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negative_numbers}" unless negative_numbers.empty?
+
+    number_set.sum
   end
 end
--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -24,6 +24,13 @@ RSpec.describe StringCalculator do
       expect { StringCalculator.add("1,-2,3") }.to raise_error("negative numbers not allowed [-2]")
       expect { StringCalculator.add("-1,2,-3") }.to raise_error("negative numbers not allowed [-1, -3]")
     end
-  end 
+  end
+
+  context 'with custom delimiter' do
+    it 'returns the sum of the numbers using the custom delimiter' do
+      expect(StringCalculator.add("//;\n4;2")).to eq(6)
+      expect(StringCalculator.add("//|\n1|5|3")).to eq(9)
+    end
+  end
   
 end
